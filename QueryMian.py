b10c0c7771a0f4781aa7568e5f2ac3a6d7fb7bf1
Light <18362927193@163.com>
2016-12-01 23:18:03 +0800

    简单的页面排序

--- QueryMian.py
+++ QueryMian.py
@@ -12,14 +12,17 @@ query = '表彰学生教育管理创新奖的决定'
 
 ConMongo = GetData.ConMongo()
 ConEs = GetData.ConEs()
+SortPage = SortPage.SortPage()
 
 result = ConEs.GetSimple(index, query)
 list = []
 for hit in result['hits']['hits']:
     PR = ConMongo.GetSimple(database, collection, hit['_id'])
     score = math.log10(PR*10000)*hit['_score']
+    # score = hit['_score']
     list.append({'url': hit['_source']['url'], 'score': score, 'content': hit['_source']['content']})
 
+list = SortPage.SortSimp(list)
 for i in list:
     print i['url'], i['score'], i['content'].encode('utf8')
 
--- SortPage.py
+++ SortPage.py
@@ -1,1 +1,12 @@
 # -*- coding:utf-8 -*-
+
+
+class SortPage(object):
+    def SortSimp(self, list):
+        for i in range(len(list)):
+            for j in range(len(list)-(i+1)):
+                if list[j]['score'] > list[j+1]['score']:
+                    temp = list[j]
+                    list[j] = list[j+1]
+                    list[j+1] = temp
+        return list
