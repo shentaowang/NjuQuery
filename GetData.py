1658cd044337210684cb80d9858ed35976d0052d
light_master <18362927193@163.com>
2017-03-09 15:08:45 +0800

    Put on the server

--- GetData.py
+++ GetData.py
@@ -4,7 +4,7 @@ import pymongo
 
 
 EsClient = Elasticsearch(['114.212.236.165:9200'])
-MongoClient = connection = pymongo.MongoClient()
+MongoClient = connection = pymongo.MongoClient(connect=False)
 
 class ConMongo(object):
     def GetSimple(self, database, collection, id):
