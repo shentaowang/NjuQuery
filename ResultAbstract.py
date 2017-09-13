d2b7fdb887b26fe8ca5b839cb1e24e94130dc9b9
Light <18362927193@163.com>
2017-09-13 17:30:35 +0800

    update the ResultAbstract

--- ResultAbstract.py
+++ ResultAbstract.py
@@ -5,13 +5,17 @@ import re
 AbstractLen = 100
 
 class Abstract(object):
-	def AbstractSimple(self,wordlist,content):
-		for tag in wordlist:
-			location = re.search(tag[0],content)
-			try:
-				start = location.start()
-				content = content[start:start+AbstractLen]
-				return content
-			except Exception as e:
-				pass
-		return None
+    def AbstractSimple(self, wordlist, content):
+        for tag in wordlist:
+            location = re.search(tag[0],content)
+            try:
+                start = location.start()
+                content = content[start:start+AbstractLen]
+                return content
+            except Exception as e:
+                pass
+        return None
+
+    def AbstractV2(self, wordlist, content):
+        pass
+
